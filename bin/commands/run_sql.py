25089231e54e928335291b64e3997c2abc39e1c4
Arnau Sanchez <pyarnau@gmail.com>
2019-08-30 12:05:06 +0200

    runsql: Use docker exec instead of docker-compose exec

--- bin/commands/run_sql.py
+++ bin/commands/run_sql.py
@@ -13,8 +13,12 @@ def run(args):
     image_name = args.image or utils.get_running_image_name()
     sql_file = args.sql_file
     utils.logger.info("Run SQL file {} for image {}".format(sql_file, image_name))
-    cmd = ["exec", "-T", "db", "psql", "-U", "dhis", "dhis2"]
-    result = utils.run_docker_compose(cmd, image_name, stdin=open(sql_file), raise_on_error=False)
+    status = utils.get_image_status(image_name)
+    if status["state"] != "running":
+        utils.logger.error("Container is not running")
+    db_container = status["containers"]["db"]
+    cmd = ["docker", "exec", "-i", db_container, "psql", "-U", "dhis", "dhis2"]
+    result = utils.run(cmd, stdin=open(sql_file), raise_on_error=False)
     if result.returncode != 0:
         utils.logger.error("Could not execute the SQL file, is the container running?")
         return 1

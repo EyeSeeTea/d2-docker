4b5856b7068d79d42270c5bcff4994f90d589e19
Arnau Sanchez <pyarnau@gmail.com>
2019-09-04 13:18:08 +0200

    Add logs -f

--- bin/commands/logs.py
+++ bin/commands/logs.py
@@ -4,10 +4,12 @@ DESCRIPTION = "Show docker logs"
 
 
 def setup(parser):
-    parser.add_argument("image", metavar="IMAGE", type=str, nargs="?", help="Hub docker image")
+    parser.add_argument("image", metavar="IMAGE", nargs="?", help="Hub docker image")
+    parser.add_argument("-f", "--follow", action="store_true", help="Follow log output")
 
 
 def run(args):
     image_name = args.image or utils.get_running_image_name()
     utils.logger.info("Show logs: {}".format(image_name))
-    utils.run_docker_compose(["logs", "-f"], image_name)
+    args = ["logs", "-f" if args.follow else None]
+    utils.run_docker_compose(filter(bool, args), image_name)

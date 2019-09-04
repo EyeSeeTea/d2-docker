75b64674a092aa8a869fc2098a74572611fed451
Arnau Sanchez <pyarnau@gmail.com>
2019-09-04 12:52:16 +0200

    Implement run-sql with interactive psql console

--- bin/commands/run_sql.py
+++ bin/commands/run_sql.py
@@ -5,20 +5,30 @@ NAME = "run-sql"
 
 
 def setup(parser):
-    parser.add_argument("-i", "--image", metavar="IMAGE", type=str, help="Docker dhis2-data image")
-    parser.add_argument("sql_file", metavar="SQL_FILE", type=str, help="SQL file")
+    parser.add_argument("-i", "--image", metavar="IMAGE", help="Docker dhis2-data image")
+    parser.add_argument(
+        "sql_file",
+        metavar="SQL_FILE",
+        nargs="?",
+        help="SQL file (if empty, open interactive psql terminal)",
+    )
 
 
 def run(args):
     image_name = args.image or utils.get_running_image_name()
     sql_file = args.sql_file
-    utils.logger.info("Run SQL file {} for image {}".format(sql_file, image_name))
+    if sql_file:
+        utils.logger.info("Run SQL file {} for image {}".format(sql_file, image_name))
+
     status = utils.get_image_status(image_name)
     if status["state"] != "running":
         utils.logger.error("Container is not running")
+
     db_container = status["containers"]["db"]
-    cmd = ["docker", "exec", "-i", db_container, "psql", "-U", "dhis", "dhis2"]
-    result = utils.run(cmd, stdin=open(sql_file), raise_on_error=False)
+    psql_cmd = ["psql", "-U", "dhis", "dhis2"]
+    cmd = ["docker", "exec", ("-i" if sql_file else "-it"), db_container, *psql_cmd]
+    result = utils.run(cmd, stdin=(open(sql_file) if sql_file else None), raise_on_error=False)
+
     if result.returncode != 0:
         utils.logger.error("Could not execute the SQL file, is the container running?")
         return 1

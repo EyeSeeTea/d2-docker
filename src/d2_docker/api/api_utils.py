d763d484dbdf3260830dab922320e140830fb02e
Arnau Sanchez <pyarnau@gmail.com>
2022-03-23 10:41:10 +0100

    Better error management

--- src/d2_docker/api/api_utils.py
+++ src/d2_docker/api/api_utils.py
@@ -1,4 +1,8 @@
+import os
+import base64
 from d2_docker.commands import list_
+from flask import jsonify
+from dotenv import dotenv_values
 
 
 class Struct(object):
@@ -19,6 +23,28 @@ def get_args_from_request(request):
     return args
 
 
+def get_auth_headers(user, password):
+    encoded_auth = base64.b64encode((user + ":" + password).encode()).decode()
+    return {"Authorization": "Basic " + encoded_auth}
+
+
 def get_container(name):
     containers = list_.get_containers()
     return next((c for c in containers if c["name"] == name), None)
+
+
+def success():
+    return jsonify(dict(status="SUCCESS"))
+
+
+def server_error(message, status=500):
+    body = jsonify(dict(status="ERROR", error=message))
+    return (body, status)
+
+
+def get_config():
+    return {
+        **dotenv_values(".flaskenv"),
+        **dotenv_values(".flaskenv.secret"),
+        **os.environ,
+    }

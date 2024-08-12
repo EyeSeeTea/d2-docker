7e5f9073d9c6d6180c003d4ab7a9846d23551dcf
Arnau Sanchez <pyarnau@gmail.com>
2024-08-12 13:10:21 +0200

    Set ownership of whole data dir to tomcat

--- src/d2_docker/config/dhis2-core-entrypoint.sh
+++ src/d2_docker/config/dhis2-core-entrypoint.sh
@@ -6,7 +6,7 @@
 # We need our custom entrypoint to perform the following different tasks:
 #  - Make files in TOMCATDIR be of user tomcat (so we can change tomcat files in pre/post scripts)
 #
-set -e  # exit on errors
+set -e # exit on errors
 
 WARFILE=/usr/local/tomcat/webapps/ROOT.war
 TOMCATDIR=/usr/local/tomcat
@@ -16,12 +16,12 @@ DATA_DIR=/data
 if [ "$(id -u)" = "0" ]; then
     if [ -f $WARFILE ]; then
         unzip -q $WARFILE -d $TOMCATDIR/webapps/ROOT
-        rm -v $WARFILE  # just to save space
+        rm -v $WARFILE # just to save space
     fi
 
     mkdir -p $DATA_DIR/apps
-    chown -R tomcat:tomcat $TOMCATDIR $DATA_DIR/apps $DHIS2HOME
-    chmod -R u=rwX,g=rX,o-rwx $TOMCATDIR $DATA_DIR/apps $DHIS2HOME
+    chown -R tomcat:tomcat $TOMCATDIR $DATA_DIR $DHIS2HOME
+    chmod -R u=rwX,g=rX,o-rwx $TOMCATDIR $DATA_DIR $DHIS2HOME
 
     # Launch the given command as tomcat, in two ways for backwards compatibility:
     if [ "$(grep '^ID=' /etc/os-release)" = "ID=alpine" ]; then

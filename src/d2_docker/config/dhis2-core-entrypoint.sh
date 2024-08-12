47533c96cf0240fdf50254e3f418ecba1dcfc0cd
Arnau Sanchez <pyarnau@gmail.com>
2024-08-12 13:11:02 +0200

    Wait for data container to finish before starting core/tomcat container

--- src/d2_docker/config/dhis2-core-entrypoint.sh
+++ src/d2_docker/config/dhis2-core-entrypoint.sh
@@ -13,11 +13,43 @@ TOMCATDIR=/usr/local/tomcat
 DHIS2HOME=/DHIS2_home
 DATA_DIR=/data
 
+debug() {
+    echo "[dhis2-core-entrypoint] $*" >&2
+}
+
+curl_status_connection_refused=7
+
+wait_for_data_container_to_finish_copy() {
+    # We must wait for the data container before starting Tomcat. As ping is not installed
+    # in the image, use curl instead.
+    local statuscode
+
+    while true; do
+        statuscode=0
+        curl -sS --connect-timeout 1 data || statuscode=$?
+
+        case $statuscode in
+        "$curl_status_connection_refused")
+            debug "data container is still running, wait"
+            sleep 2
+            continue
+            ;;
+        *)
+            debug "data container has finished, continue"
+            break
+            ;;
+        esac
+    done
+
+}
+
 if [ "$(id -u)" = "0" ]; then
     if [ -f $WARFILE ]; then
         unzip -q $WARFILE -d $TOMCATDIR/webapps/ROOT
         rm -v $WARFILE # just to save space
     fi
+
+    wait_for_data_container_to_finish_copy
 
     mkdir -p $DATA_DIR/apps
     chown -R tomcat:tomcat $TOMCATDIR $DATA_DIR $DHIS2HOME
